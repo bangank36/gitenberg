c4ffd3f1cc5ae26cab3c5a3ecdcae51de5851cfd
bangank36 <bangank36@gmail.com>
2023-05-28 16:21:03 +0700

    - Override image renderer

---
+++ src/formatters/markdown-it-renderer.js
@@ -0,0 +1,13 @@
+export const gitenbergRenderer = function (md) {
+    md.renderer.rules.image = function (tokens, idx, options, env, slf) {
+		const token = tokens[idx];
+		const srcAttribute = token.attrGet('src');
+		if (srcAttribute && srcAttribute.includes('shields.io')) {
+			return `<!-- wp:gitenberg/shields-badge --><div class="wp-block-gitenberg-shields-badge">
+					<img src="https://shields.io/badge/style-flat--square-green?label=healthinesses&amp;logo=appveyor&amp;logoColor=violet&amp;logoWidth=40&amp;labelColor=abcdef&amp;color=fedcba&amp;style=flat-square"/>
+				</div><!-- /wp:gitenberg/shields-badge -->`;
+		}
+	  
+		return `${slf.renderToken(tokens, idx, options)}`;
+	}
+}
--- src/formatters/markdown.js
+++ src/formatters/markdown.js
@@ -1,12 +1,15 @@
 import TurndownService from 'turndown';
 import MarkdownIt from 'markdown-it';
 import { gfm } from './turndown-gfm';
+import { gitenbergRenderer } from './markdown-it-renderer';
 
 const turndown = new TurndownService( {
 	bulletListMarker: '-',
 } );
-
 gfm( turndown );
+
+const md = new MarkdownIt();
+gitenbergRenderer(md);
 
 turndown.addRule( 'listItem', {
 	filter: 'li',
@@ -40,10 +43,23 @@ export function load( content, parser, rawHandler ) {
 		return parser( content.value );
 	}
 
-	const md = new MarkdownIt();
+	const htmlContent = `
+	<p><!-- wp:gitenberg/shields-badge --><div class="wp-block-gitenberg-shields-badge">
+					<img src="https://shields.io/badge/style-flat--square-green?label=healthinesses&amp;logo=appveyor&amp;logoColor=violet&amp;logoWidth=40&amp;labelColor=abcdef&amp;color=fedcba&amp;style=flat-square"/>
+				</div><!-- /wp:gitenberg/shields-badge --></p>
+<p><!-- wp:gitenberg/shields-badge --><div class="wp-block-gitenberg-shields-badge">
+					<img src="https://shields.io/badge/style-flat--square-green?label=healthinesses&amp;logo=appveyor&amp;logoColor=violet&amp;logoWidth=40&amp;labelColor=abcdef&amp;color=fedcba&amp;style=flat-square"/>
+				</div><!-- /wp:gitenberg/shields-badge --></p>
+<p><!-- wp:gitenberg/shields-badge --><div class="wp-block-gitenberg-shields-badge">
+					<img src="https://shields.io/badge/style-flat--square-green?label=healthinesses&amp;logo=appveyor&amp;logoColor=violet&amp;logoWidth=40&amp;labelColor=abcdef&amp;color=fedcba&amp;style=flat-square"/>
+				</div><!-- /wp:gitenberg/shields-badge --></p>
+<p><img src="https://placehold.co/600x400/EEE/31343C" alt=""></p>
+	`;
+
+	console.log(htmlContent);
 
 	// Raw HTML - do our best
-	return rawHandler( { HTML: md.render( content.value ) } );
+	return rawHandler( { HTML: htmlContent } );
 }
 
 /**
