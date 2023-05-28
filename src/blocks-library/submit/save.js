8e264c986c238f7402406404b0207f7fb4382118
bangank36 <bangank36@gmail.com>
2023-05-28 09:56:43 +0700

    Clean up save.js

--- src/blocks-library/submit/save.js
+++ src/blocks-library/submit/save.js
@@ -1,14 +1,8 @@
 /**
  * WordPress Dependencies
  */
-import { RawHTML } from '@wordpress/element';
 import { useBlockProps } from '@wordpress/block-editor';
 import { addQueryArgs } from '@wordpress/url';
-
-/**
- * Custom Dependencies
- */
-import { convertSubmitBlockToCF7Shortcode } from '../utils/convert-to-cf7-shortcode';
 
 export default function save( props ) {
 	const { label, logo, logoColor, logoWidth, labelColor, color, style } = props.attributes;
